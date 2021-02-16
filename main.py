a32d9c533ca52bd525ff9707b178c30a835d3684
Casper2nd <CasNoukSuus73>
2021-02-16 21:36:39 +0100

    it now removes the remainder from the fraction

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-
-#den stands for denominator and nom for nominator
+# den stands for denominator and nom for nominator
 def fraction(number):
+    if number.is_integer():
+        print(number)
+        return
     nomMax = int(number) + 1
     nomMin = int(number)
     nom = nomMax + nomMin
     denMax = 1
     denMin = 1
     den = denMax + denMin
-    reached = False
-    for x in range(10000):
-        if(number > nom/den):
+
+    for x in range(100000):
+        if number > nom / den:
             nomMin = nom
             nom += nomMax
             denMin = den
             den += denMax
-        elif(number < nom / den):
+        elif number < nom / den:
             nomMax = nom
             nom += nomMin
             denMax = den
@@ -23,30 +25,37 @@ def fraction(number):
             tn = formatFraction(nom, den)
             nom = tn[0]
             den = tn[1]
-            print("%s / %d" % (nom, den))
-            reached = True
+            number2 = nom // den
+            nom -= number2*den
+            print("%d %d/%d" % (number2, nom, den))
+            return
+    tn = formatFraction(nom, den)
+    nom = tn[0]
+    den = tn[1]
+    number2 = nom // den
+    nom -= number2 * den
+    print("didn't reach, but closest is %d/%d" % (nom, den))
+    print(nom / den)
 
-            break
-    if(not reached):
-        tn = formatFraction(nom, den)
-        nom = tn[0]
-        den = tn[1]
-        print("didn't reach, but closest is %s / %d" % (nom, den))
-        print(nom / den)
 
-def formatFraction(T,N):
-    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+def formatFraction(T, N):
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103,
+              107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223,
+              227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347,
+              349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+              467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607,
+              613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743,
+              751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883,
+              887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
     for x in primes:
-        if(T%x == 0 and N%x == 0):
+        if T % x == 0 and N % x == 0:
             T /= x
             N /= x
-            tn = formatFraction(T,N)
+            tn = formatFraction(T, N)
             T = tn[0]
             N = tn[1]
             break
-    return [T,N]
-
-
+    return [T, N]
 
 
 while True:
@@ -54,6 +63,7 @@ while True:
         n = input("enter a number\n")
         n = float(n)
         break
-    except:
+    except ValueError:
         print("wrong number")
+
 fraction(n)
